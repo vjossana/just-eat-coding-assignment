d8a77d07bf55599686266aaa76fc4bc41db36e5f
Vjosana Kurti <vjosana@Vjosanas-MacBook-Air.local>
2026-03-25 12:37:43 +0000

    Add Restaurant class with name, rating, cuisines and address properties

---
+++ restaurant.py
@@ -0,0 +1,14 @@
+class Restaurant:
+    """Represents a single restaurant with desired fields"""
+    def __init__(self, name, rating, cuisines, address):
+        self.name = name
+        self.rating = rating
+        self.cuisines = self.cuisines
+        self.address = address
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Rating: {self.rating}")
+        print(f"Cuisines: {', '.join(self.cuisines)}")
+        print(f"Address: {self.address}")
+        print("---")
