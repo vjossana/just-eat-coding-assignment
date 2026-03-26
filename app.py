65d8d95e2a83e22059e68294d4d1b7305f167cf9
Vjosana Kurti <vjosana@Vjosanas-MacBook-Air.local>
2026-03-26 10:51:25 +0000

    Add UK postcode format validation

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from restaurant_service import RestaurantService
 
 app = Flask(__name__)
 
+#Postcode format validation
+def valid_postcode(postcode):
+    pattern = "^[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}$"
+    return re.match(pattern, postcode)
+
 #Loading page and form submission
 @app.route("/", methods=["GET" , "POST"])
 def index():
@@ -13,9 +18,12 @@ def index():
     #Fetch restaurants if form submitted
     if request.method == "POST":
         postcode = request.form.get("postcode", "").strip().upper()
+
         #Dont call API if postcode empty
         if not postcode:
             error = "Please enter a valid postcode"
+        elif not valid_postcode(postcode):
+            error = "Please enter a valid UK postcode (e.g. EC4M 7RF)"
         else:
             service = RestaurantService()
             restaurants = service.get_restaurants(postcode)
@@ -25,5 +33,5 @@ def index():
             
         return render_template("index.html", restaurants=restaurants, postcode=postcode, error=error)
     
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
